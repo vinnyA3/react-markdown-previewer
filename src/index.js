25349249a279dba5ad950117aec2f74cda90b114
vinnya3 <vinaceto@yahoo.com>
2016-02-08 12:55:41 -0500

    imported components

--- src/index.js
+++ src/index.js
@@ -4,18 +4,28 @@ import ReactDom from "react-dom";
 import Marked from "marked";
 //sass
 require("../sass/styles.scss");
+//components
+import InputText from "./components/input-text";
+import OutputText from "./components/output-text";
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      markdown: Marked('I am using __Markdown__')
+      //markdown: Marked('I am using __Markdown__')
+      markdown: ''
     }
   }
+
+  inputChange(markdown){
+      this.setState({ markdown });
+  }
+
   render(){
     return(
-      <div>
-        {this.state.markdown}
+      <div className="component-container">
+        <InputText onMarkdownChange={(markdown) => this.inputChange(markdown)} />
+        <OutputText markdownText={this.state.markdown} />
       </div>
     );
   }
