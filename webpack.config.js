db19b3d63b85d2c40112493ec3a086226bf488b9
vinnya3 <vinaceto@yahoo.com>
2016-02-08 01:15:15 -0500

    new files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry:[
+    './src/index.js'
+  ],
+  output:{
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module:{
+    loaders:[{
+      exclude:'/node_modules/',
+      loader: 'babel'
+    },
+    {
+      test: '/\.scss$/',
+      loaders: ['style','css','sass']
+    }]
+  },
+  resolve: {
+    extenstions:['','.js','.jsx']
+  },
+  devServer:{
+    historyApiFallback: true,
+    contentBase: '/'
+  }
+};
