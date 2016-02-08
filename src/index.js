582852a35905b4d2578f5ee6a2de84c9174d06dc
vinnya3 <vinaceto@yahoo.com>
2016-02-08 02:41:21 -0500

    required markdown, added state to constructor, added state to rendered markup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
+//require markdown package
+import Marked from "marked";
 //sass
-require("../sass/styles.scss")
-
+require("../sass/styles.scss");
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      markdown: Marked('I am using __Markdown__')
+    }
+  }
   render(){
     return(
       <div>
-        <h1>Hello Annie!</h1>
+        {this.state.markdown}
       </div>
     );
   }
