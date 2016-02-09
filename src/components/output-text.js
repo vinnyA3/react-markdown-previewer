8de823c50a0511dd58f2d7c1c6f0d220c82909d4
vinnya3 <vinaceto@yahoo.com>
2016-02-09 01:58:21 -0500

    removed console.log

--- src/components/output-text.js
+++ src/components/output-text.js
@@ -3,6 +3,7 @@ import React from "react";
 const OutputText = (props) => {
     return(
         <div className = "box">
+            <h1>Output</h1>
             <div className="output-text">
               {props.markdownText}
             </div>
